318640e242437cc5172a3602fa87f10d04538c16
Modestas <modestask97@gmail.com>
2019-11-27 10:52:57 +0000

    test structure

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// adapts enzyme to work with correct react version
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without error", () => {});
+test("renders increment button", () => {});
+test("renders counter display", () => {});
+test("counter starts at 0", () => {});
+test("clicking button increments counter in display", () => {});
